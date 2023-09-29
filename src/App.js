731e7c147766900cb6d43058c10602ae6769955d
tapsaca <98756983+tapsaca@users.noreply.github.com>
2023-09-29 14:08:21 +0300

    Use the API to fetch all movies

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import api from './api/axiosConfig'
+import { useEffect, useState } from 'react'
 
-function App() {
+const App = () => {
+  const [movies, setMovies] = useState()
+
+  const getMovies = async () => {
+    try {
+      const response = await api.get('/api/movies')
+      console.log(response.data)
+      setMovies(response.data)
+    } catch(error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    getMovies()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div></div>
+  )
 }
 
-export default App;
+export default App
